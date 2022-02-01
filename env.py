55152b474da3107e7808581d14ef0fe57bd3e95c
surba778 <surba778@gmail.com>
2022-02-01 19:14:01 +0000

    Procfile is updated

---
+++ env.py
@@ -0,0 +1,4 @@
+import os
+
+os.environ["DATABASE_URL"] = "postgres://lvbpsjxdeieeae:25ccfeb195b1dea67f8449ccceab16278c795d18ededf4685429d6f1a7c99f72@ec2-52-49-56-163.eu-west-1.compute.amazonaws.com:5432/de8tjk0hpoi424"
+os.environ["SECRET_KEY"] = "randomSecretKey2022"
