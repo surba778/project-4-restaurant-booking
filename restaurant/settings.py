a90a0ffc7f4b88f67aa096915f43bf973731340d
surba778 <surba778@gmail.com>
2022-05-03 18:30:08 +0000

    set debug to false in settings.py

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=True, cast=bool) # True
+DEBUG = False
 
 ALLOWED_HOSTS = ["booking2022.herokuapp.com", "localhost"]
 
